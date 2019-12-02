839c4e9a013b486d5efd0637fbb61fe2a17ec2a8
Ruri <unryuri4140@gmail.com>
2019-12-02 20:28:59 +0900

    Movie function almost done

--- src/pages/Selector.js
+++ src/pages/Selector.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import { MovieSelector } from '../MovieSelect';
 import { TheaterSelector } from '../TheaterSelect';
+import { DateSelector } from '../DateSelect';
 import { TimeSelector } from '../TimeSelect';
+import './Selector.css';
 
 class Selector extends React.Component {
     render() {
         return (
             <div className="Selector">
                 <div className="LeftSelector">
-                    <TimeSelector></TimeSelector>
+                    <DateSelector></DateSelector>
                     <TheaterSelector></TheaterSelector>
                     <MovieSelector></MovieSelector>
-                </div>   
+                </div>
+                <div className="RightSelector">
+                    <TimeSelector></TimeSelector>
+                </div>
             </div>
         );
     }
